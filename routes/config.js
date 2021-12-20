0fccf5452dc76a2f42f6ed1f4ead3fca78d452a3
LMeucci <l.meucci.89@gmail.com>
2021-12-20 18:11:22 +0100

    PR section added, needs check on selection

--- routes/config.js
+++ routes/config.js
@@ -7,8 +7,10 @@ router.get('/configurazione', (req, res) => {
         info: "Questa è la sezione dedicata all'impostazione e regolazione dei tuoi dispositivi. Dopo aver regolato i vari dispositivi disponibili è possibile salvare la configurazione attuale per poterla utilizzare in seguito. Per fare ciò è necessario associargli un nome e premere il tasto salva.",
         led: "Area LED",
         infoLED: "In questa area è possibile regolare la luminosità dei LED. Sono previsti 10 livelli (1-10) per ogni LED.",
+        infoLED2: " Se viene impostato un fotoresistore per un determinato LED non è possibile impostare un valore manuale per quello stesso LED finchè non viene selezionato il valore \"Nessuno\" per quel fotoresistore.",
         pr: "Area Fotoresistori",
-        infoPR: "In questa area è possibile associare ognuno dei fotoresistori installati ad uno o più LED presenti in un ambiente, in modo da regolarne la luminosità in maniera automatica.",
+        infoPR: "In questa area è possibile associare ogni LED uno dei fotoresistori installati in modo da regolarne la luminosità in maniera automatica.",
+        infoPR2: " Fintanto che per un LED è selezionato un fotoresistore, per quello stesso LED non può essere impostato un valore manualmente tramite i suoi pulsanti.",
         save: "Area di salvataggio",
         infoSave: "In questa area è possibile salvare le impostazioni scelte nelle aree soprastanti. E' richiesto l'inserimento di un nome da associare alla configurazione attuale per procedere al salvataggio."
     });
